d440cb5cca89825267cc69e165319f4506876c61
sticks8416 <sticks123@naver.com>
2020-03-14 00:56:53 +0900

    03140056

--- test/src/main/java/board/controller/OrderController.java
+++ test/src/main/java/board/controller/OrderController.java
@@ -1,17 +1,29 @@
 package board.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import board.domain.HacchuuVO;
+import board.service.OrderService;
 
+@Controller
+@SessionAttributes("orderVO")
 public class OrderController {
+	@Resource
+	private OrderService orderService;
+
+	public void setOrderService(OrderService OrderService) {
+		this.orderService = OrderService;
+	}
 	@RequestMapping(value = "/board/chuumonn", method = RequestMethod.GET)
 	public String chuumonn(ModelMap model, HttpSession session, HttpServletRequest req, String kensaku)
 			throws Exception {
--- test/src/main/java/board/service/OrderServiceImpl.java
+++ test/src/main/java/board/service/OrderServiceImpl.java
@@ -1,5 +1,19 @@
 package board.service;
 
-public class OrderServiceImpl {
+import org.springframework.stereotype.Service;
 
+import board.dao.OrderDao;
+import board.domain.OrderVO;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+	private OrderDao orderDao;
+
+	public OrderDao getOrderDao() {
+		return orderDao;
+	}
+
+	public void setOrderDao(OrderDao orderDao) {
+		this.orderDao = orderDao;
+	}
 }
